88215626b79c554cd0a479ec3b3b03d06e35991f
ntrvic <vic.gda@gmail.com>
2014-05-30 09:21:44 -0400

    first commit

---
+++ rss_feed.rb
@@ -0,0 +1,25 @@
+require 'rss'
+require 'open-uri'
+require 'httparty'
+
+
+@default_request_headers = {
+'Content-Type' => 'application/x-www-form-urlencoded',
+'Accept-Language' => 'en-US,en;q=0.5',
+'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) Gecko/20100101 Firefox/28.0',
+'Referer' => 'https://dev.workmarket.com/',
+'Accept-Encoding' => 'gzip,deflate'
+}
+url = 'http://newyork.craigslist.org/search/aap/mnh?housing_type=1&maxAsk=3000&minAsk=800&nh=131&nh=136&nh=137&s=0&sale_date=-&format=rss'
+
+open(url) do |rss|
+  feed = RSS::Parser.parse(rss)
+  puts "Title: #{feed.channel.title}"
+  puts "Link: #{feed.channel.link}"
+  feed.items.each do |item|
+    puts "Item: #{item.link}"
+    response = HTTParty.get(item.link, :headers => @default_request_headers)
+    puts response
+  end
+end
